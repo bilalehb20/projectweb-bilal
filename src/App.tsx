18e24837cf7fb89526f650915d4f01c2ba6f6771
Bilal Meziani <bilal.meziani@student.ehb.be>
2025-05-13 12:46:42 +0200

    Initial commit: Rick and Morty Explorer setup

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div className="container mx-auto p-4">
+      <h1 className="text-3xl font-bold text-blue-500">Rick and Morty Explorer</h1>
+      <p className="text-green-500">Test TailwindCSS</p>
+    </div>
+  );
+}
+
+export default App;
